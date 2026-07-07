edc8c0cd3320586650f57908adfac9e9477f45bb
Michael <ndudimichael06@gmail.com>
2026-07-07 10:23:36 +0100

    fixed views error

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('businesses/', views.business_views),
-    path('businesses/<int:business_id>/', views.get_business_by_id),
+    path('business/', views.business_views),
+    path('business/<int:business_id>/', views.get_business_by_id),
     path('events/<int:event_id>/', views.list_events_by_id),
     path('events/', views.event_views),
     path('events/<int:event_id>/ticket-types/', views.ticket_type_views),
